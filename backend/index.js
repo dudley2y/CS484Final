a387eae01a4b090ccca3e4454dc139c4a58cb274
Joshua Dudley <dudley.joshua789@gmail.com>
2021-09-20 00:39:06 -0700

    i think passport.js stuff works

--- backend/index.js
+++ backend/index.js
@@ -4,52 +4,78 @@ const bodyParser = require('body-parser');
 const sqlite3 = require("sqlite3")
 const bcrypt = require('bcrypt');
 const passport = require("passport");
-const initPassport = require("./passport-config.js");
+const LocalStrategy =  require("passport-local");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(passport.initialize());
 
 var db = new sqlite3.Database('./test.db')
 
-app.post('/login', (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const sqlSearch = `SELECT * FROM userLogin WHERE username = ?;`
-    db.get(sqlSearch,[username], (err,row) => {
-        if(row == null){ res.send("No user found")} // doesn't exist
-        bcrypt.compare(password, row.password, function(err,result){
-            res.send("Authenticated: " + result)
-        })
-    });
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done){
+        const sqlSearch = `SELECT * FROM userLogin WHERE username = ?;`
+        db.get(sqlSearch,[username], (err,row) => {
+            // user isn't found in database
+            if(row == null){ return done(null, false, { message: "No user with that email"})} 
+            else{  // we found user in database
+                bcrypt.compare(password, row.password, function(err,result){
+                    if(result){ // password matches
+                        return done(null, row)
+                    }
+                    else{
+                        return done(null, false, {message: "Password inccorect"})
+                    }
+                })
+            } 
+        });
+    }
+));
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.send(req.user);
+  });
 
 app.post('/register', (req,res) =>{
     const name = req.body.name;
     const username = req.body.username;
     const password = req.body.password;
 
-    console.log("RECIEVED: ",name, username, password)
+    console.log("RECIEVED: ",name, username, password, " from register")
 
+    // search to see if user already exists in databsae
     const sqlSearch = `SELECT * FROM userLogin WHERE username = ?;`
 
     db.get(sqlSearch, [username], (err, row) => {
-        if(row!=null){ 
+        if(row!=null){   // user already exists
             res.send("user already exists") 
-            return
+        }else{          // user doesn't exist so we add to database
+            bcrypt.hash(password, 10, function(err, hash){
+                if(err){
+                    res.send(err)
+                    return err;
+                }
+                console.log("Created hash: ",hash)
+                const sqlQuery = `INSERT INTO userLogin(name, username, password) VALUES("${name}", "${username}", "${hash}");`
+                db.run(sqlQuery)
+                res.send("Successfully added")
+                return hash
+            });
         }
     });
 
-    bcrypt.hash(password, 10, function(err, hash){
-        if(err){
-            return err;
-        }
-        console.log("Created hash: ",hash)
-        const sqlQuery = `INSERT INTO userLogin(name, username, password) VALUES("${name}", "${username}", "${hash}");`
-        db.run(sqlQuery)
-        return hash
-    });
 });
 
 app.listen(5000, () => {
--- backend/passport-config.js
+++
@@ -1,1 +0,0 @@
-const localStrategy = require("passport-local").Strategy;
