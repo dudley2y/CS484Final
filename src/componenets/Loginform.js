65152d09f93d30a7bc1cc54b1aa86362040d4321
Joshua Dudley <dudley.joshua789@gmail.com>
2021-09-23 23:08:51 -0500

    login redone

--- src/componenets/Loginform.js
+++ src/componenets/Loginform.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {useState} from 'react';
+import { Form } from 'semantic-ui-react';
 
 const Loginform = () => {
     const [username, setUsername] = useState("")
@@ -29,17 +30,14 @@ const Loginform = () => {
 
     return(
         <div>
-            <h1>Hello from Log in</h1>
-            <form className = "ui fluid form" onSubmit={loginAction}>
+            <Form onSubmit={loginAction}>
+                <Form.Group widths = "equal"> 
+                    <Form.Input label = "Username" type = "text" placeholder = "Username" name = "username" onChange = {(evt) => setUsername(evt.target.value)}/>
+                    <Form.Input label = "Password" type = "password" placeholder = "Password" name = "password" onChange = {(evt) => setPassword(evt.target.value)}/>   
+                </Form.Group>
+                <Form.Button type = "submit">Login!</Form.Button>
+            </Form>
 
-                <div className="ui input focus">
-                    <input name = "username" type="text" placeholder="Username"  onChange = {(evt) => setUsername(evt.target.value)}/>
-                </div>
-                <div className="ui input focus">
-                    <input name = "password" type="text" placeholder="Password"  onChange = {(evt) => setPassword(evt.target.value)}/>
-                </div>
-                <button className="ui button" type="submit">Submit</button>
-            </form>
             <h1>{response}</h1>
         </div>
     )
