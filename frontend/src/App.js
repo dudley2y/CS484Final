c64c3bccf7be63a89318eda6994c30aec26bdf3d
binJamin <jraffe2@uic.edu>
2021-10-03 13:34:19 -0500

    Attempting Docker sync
    Still in testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import React, {useState, useEffect } from 'react';
+import Main from "./componenets/Main.js"
 import Login from "./componenets/Login.js"
 import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Main/>
     </div>
   );
 }
---
+++ frontend/src/componenets/Main.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const Main = () => {
+
+    return(
+        <div >
+            <h1>Yoote</h1>
+        </div>
+    )
+
+}
+export default Main;
