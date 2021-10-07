af32be8a0ddd6a4bc4ba81092f7a3dc8737a3a83
Joshua Dudley <dudley.joshua789@gmail.com>
2021-10-07 13:43:36 -0500

    idk routes not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
-import React, {useState, useEffect } from 'react';
-import Login from "./componenets/Login.js"
-import axios from 'axios';
+import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
+import Login from "./componenets/Login.js";
+import AuthSpotify from './componenets/AuthSpotify.js';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login/>
+          </Route>
+          <Route path="/spotify">
+            <AuthSpotify/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
